088270d6a06ae550240d009c1644f3c6e9681b3b
Pradeep Reddy Raamana <raamana@gmail.com>
2017-08-08 14:35:29 -0400

    initial trials for parcellate

--- graynet/parcellate.py
+++ graynet/parcellate.py
@@ -13,5 +13,8 @@ right_hemi = os.path.join(atlas_dir,'surf','rh.orig')
 right_coords, right_faces, right_info = nib.freesurfer.io.read_geometry(right_hemi, read_metadata=True)
 
 num_vertices_left_hemi = left_coords.shape[0]
+num_vertices_right_hemi = right_coords.shape[0]
+assert num_vertices_left_hemi == num_vertices_right_hemi
+
 wholebrain_coords = np.vstack((left_coords, right_coords))
 wholebrain_faces = np.vstack((left_faces, right_faces + num_vertices_left_hemi))
