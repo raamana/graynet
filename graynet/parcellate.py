0eed93e93eec86c56c97b4a00fbd18edfe1502ed
Pradeep Reddy Raamana <praamana@research.baycrest.org>
2017-08-09 18:50:42 -0400

    attempting to write the IO for parcellation related stuff.

--- graynet/parcellate.py
+++ graynet/parcellate.py
@@ -1,20 +1,53 @@
 
 import os
+from os.path import join as pjoin
 import sys
+import socket
 import numpy as np
 import nibabel as nib
 
-atlas_dir = '/mnt/opt/freesurfer/subjects/fsaverage'
 
-left_hemi = os.path.join(atlas_dir,'surf','lh.orig')
-left_coords, left_faces, left_info = nib.freesurfer.io.read_geometry(left_hemi, read_metadata=True)
+if socket.gethostname().startswith('SQuark'):
+    fs_subjects_dir = '/mnt/opt/freesurfer/subjects'
+else:
+    fs_subjects_dir = '/mnt/opt/freesurfer/subjects'
 
-right_hemi = os.path.join(atlas_dir,'surf','rh.orig')
-right_coords, right_faces, right_info = nib.freesurfer.io.read_geometry(right_hemi, read_metadata=True)
+atlas_name = 'fsaverage'
+atlas_dir = pjoin(fs_subjects_dir, atlas_name)
 
-num_vertices_left_hemi = left_coords.shape[0]
-num_vertices_right_hemi = right_coords.shape[0]
-assert num_vertices_left_hemi == num_vertices_right_hemi
+hemi_list = ['lh', 'rh']
+atlas, coords, faces, info = dict(), dict(), dict(), dict()
 
-wholebrain_coords = np.vstack((left_coords, right_coords))
-wholebrain_faces = np.vstack((left_faces, right_faces + num_vertices_left_hemi))
+for hemi in hemi_list:
+    hemi_path = os.path.join(atlas_dir,'surf','lh.orig')
+    coords[hemi], faces[hemi], info[hemi] = nib.freesurfer.io.read_geometry(hemi_path, read_metadata=True)
+
+num_vertices_left_hemi = coords['lh'].shape[0]
+
+coords['whole'] = np.vstack((coords['lh'], coords['rh']))
+faces['whole'] = np.vstack((faces['lh'], faces['rh'] + num_vertices_left_hemi))
+
+# labels, ctab, names = nib.freesurfer.io.read_annot(pjoin(mmp_fs,'lh.HCP-MMP1.annot'))
+
+# reading annotations
+annot = dict()
+for hemi in hemi_list:
+    annot[hemi] = dict()
+    annot_path = pjoin(atlas_dir, 'label', '{}.aparc.annot'.format(hemi))
+    annot[hemi]['labels'], annot[hemi]['ctab'], annot[hemi]['names'] = nib.freesurfer.io.read_annot(annot_path, orig_ids=True)
+
+labels_to_remove = ['corpuscallosum', 'unknown']
+null_label= 0
+
+def ismember(A, B):
+    B_unique_sorted, B_idx = np.unique(B, return_index=True)
+    B_in_A_bool = np.in1d(B_unique_sorted, A, assume_unique=True)
+
+for hemi in hemi_list:
+    cortex_label_path = pjoin(atlas_dir, 'label', '{}.cortex.label'.format(hemi))
+    cortex_label = nib.freesurfer.io.read_label(cortex_label_path)
+
+    mask_for_cortex = np.in1d(annot[hemi]['labels'], cortex_label, assume_unique=True)
+
+
+print('lets do something')
