7edd4619fb7ba6ba51e4e217823ca8c1085d9b69
Pradeep Reddy Raamana <praamana@research.baycrest.org>
2017-09-10 10:43:19 -0400

    processing status checker - broader scope [skip ci]

--- scripts/gather_datasets.py
+++ scripts/gather_datasets.py
@@ -21,10 +21,11 @@ dataset_name = '4RTNI' # 'PPMI' #
 
 # subject_id_list = pjoin(target_list_dir, 'graynet.compute.list')
 
-base_feature = 'freesurfer_thickness' # 'freesurfer_curv' #  'freesurfer_thickness'
+base_feature_list = [ 'freesurfer_thickness', 'freesurfer_curv' ] #  'freesurfer_thickness'
 atlas = 'GLASSER2016' # 'FSAVERAGE' # 'GLASSER2016' #
 fwhm = 10
 node_size = None
+num_links_expected = 360*(360-1)/2
 
 histogram_dist = np.array([
     'chebyshev', 'chebyshev_neg', 'chi_square',
@@ -38,61 +39,72 @@ histogram_dist = np.array([
 
 incomplete_processing, comb_nan_values = dict(), dict()
 
-for weight_method in histogram_dist: # ['chi_square', 'cosine', 'euclidean', 'histogram_intersection']:
-    # print('Gathering data for {}'.format(weight_method))
+for base_feature in base_feature_list:
+    incomplete_processing[base_feature] = dict()
+    comb_nan_values[base_feature] = dict()
 
-    expt_id = '{}_{}_smoothing{}_size{}_edgeweight_{}'.format(base_feature, atlas, fwhm, node_size,
-                                                              weight_method)
+    for weight_method in histogram_dist: # ['chi_square', 'cosine', 'euclidean', 'histogram_intersection']:
+        # print('Gathering data for {}'.format(weight_method))
 
-    flag_nan_exists = False
-    flag_incomplete = False
-    dataset = MLDataset()
+        expt_id = '{}_{}_smoothing{}_size{}_edgeweight_{}'.format(base_feature, atlas, fwhm, node_size,
+                                                                  weight_method)
 
-    incomplete_processing[weight_method] = dict()
-    comb_nan_values[weight_method] = dict()
-    for ds_name in dataset_list:
-        proc_dir = pjoin(base_dir, dataset_name, 'processed')
-        out_dir = pjoin(proc_dir, 'graynet', '{}_{}_fwhm{}'.format(base_feature, atlas, fwhm))
+        flag_nan_exists = False
+        flag_incomplete = False
+        flag_unexpected = False
+        dataset = MLDataset()
 
-        meta_list = pjoin(proc_dir, 'target_lists', 'meta_{}.csv'.format(ds_name))
-        sample_ids, classes = run_workflow.get_metadata(meta_list)
+        incomplete_processing[base_feature][weight_method] = dict()
+        comb_nan_values[base_feature][weight_method] = dict()
+        for ds_name in dataset_list:
+            proc_dir = pjoin(base_dir, ds_name, 'processed')
+            out_dir = pjoin(proc_dir, 'graynet', '{}_{}_fwhm{}'.format(base_feature, atlas, fwhm))
 
-        incomplete_processing[weight_method][ds_name] = list()
-        comb_nan_values[weight_method][ds_name] = list()
-        for sample in sample_ids:
-            feat_path = pjoin(out_dir, sample, '{}_graynet.csv'.format(expt_id))
-            if pexists(feat_path):
-                data = np.genfromtxt(feat_path)
-                idx_nan = np.isnan(data)
-                local_flag_nan_exists = np.count_nonzero(idx_nan) > 0
-                if local_flag_nan_exists:
-                    flag_nan_exists = True
-                    comb_nan_values[weight_method][ds_name].append(sample)
-                    # print('NaNs found for {} {} {}'.format(ds_name, weight_method, sample))
+            meta_list = pjoin(proc_dir, 'target_lists', 'meta_{}.csv'.format(ds_name))
+            sample_ids, classes = run_workflow.get_metadata(meta_list)
+
+            incomplete_processing[base_feature][weight_method][ds_name] = list()
+            comb_nan_values[base_feature][weight_method][ds_name] = list()
+            for sample in sample_ids:
+                feat_path = pjoin(out_dir, sample, '{}_graynet.csv'.format(expt_id))
+                if pexists(feat_path):
+                    data = np.genfromtxt(feat_path)
+                    idx_nan = np.logical_not(np.isfinite(data))
+                    local_flag_nan_exists = np.count_nonzero(idx_nan) > 0
+                    if local_flag_nan_exists:
+                        flag_nan_exists = True
+                        comb_nan_values[base_feature][weight_method][ds_name].append(sample)
+                        # print('NaNs found for {} {} {}'.format(ds_name, weight_method, sample))
+                    elif len(data) >= num_links_expected:
+                        dataset.add_sample(sample, data, classes[sample], class_id=classes[sample])
+                    else:
+                        flag_unexpected = True
+                        incomplete_processing[base_feature][weight_method][ds_name].append(sample)
                 else:
-                    dataset.add_sample(sample, data, classes[sample], class_id=classes[sample])
-            else:
-                flag_incomplete = True
-                # print('processing incomplete for {} {} {}'.format(ds_name, weight_method, sample))
-                incomplete_processing[weight_method][ds_name].append(sample)
+                    flag_incomplete = True
+                    incomplete_processing[base_feature][weight_method][ds_name].append(sample)
+                    # print('processing incomplete for {} {} {}'.format(ds_name, weight_method, sample))
 
-    if flag_nan_exists or flag_incomplete:
-        print('NaNs/processing incmplete for {} - totally skipping it.'.format(weight_method))
-    else:
-        print(' {} {} - fully usable.'.format(ds_name, weight_method))
-        dataset.description = 'weight {}'.format(weight_method)
-        out_path = pjoin(out_dir,'{}.MLDataset.pkl'.format(weight_method))
-        dataset.save(out_path)
+        if flag_nan_exists or flag_incomplete or flag_unexpected:
+            pass
+            # print('{:20} {:25} - processing unusable; totally skipping it.'.format(base_feature, weight_method))
+        else:
+            print('{:20} {:5} {:25} - fully usable.'.format(base_feature, ds_name, weight_method))
+            dataset.description = 'weight {}'.format(weight_method)
+            out_path = pjoin(out_dir,'{}.MLDataset.pkl'.format(weight_method))
+            dataset.save(out_path)
 
-# saving
-with open(pjoin('.', 'incomplete_processing.pkl'), 'wb') as ipf:
-    pickle.dump([incomplete_processing, comb_nan_values], ipf)
+    # saving
+    with open(pjoin(out_dir, 'incomplete_unusable_processing.pkl'), 'wb') as ipf:
+        pickle.dump([incomplete_processing, comb_nan_values], ipf)
 
-# reading
-with open(pjoin('.', 'incomplete_processing.pkl'), 'rb') as ipf:
-    incomplete_processing, comb_nan_values = pickle.load(ipf)
+    # reading
+    with open(pjoin(out_dir, 'incomplete_unusable_processing.pkl'), 'rb') as ipf:
+        incomplete_processing, comb_nan_values = pickle.load(ipf)
 
-for ds_name in dataset_list:
-    for wm in histogram_dist:
-        print('{} {:25} {:5} {:5}'.format(ds_name, wm,
-            len(incomplete[weight_method][ds_name]), len(comb_nan[weight_method][ds_name])))
+    # results
+    for ds_name in dataset_list:
+        for wm in histogram_dist:
+            print('{:20} {:5} {:25} {:5} {:5}'.format(base_feature, ds_name, wm,
+                len(incomplete_processing[base_feature][weight_method][ds_name]),
+                len(comb_nan_values[base_feature][weight_method][ds_name])))
