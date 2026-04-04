2ef84588375004db0b4fa7105e1b6c433c2a075a
Pradeep Reddy Raamana <raamana@gmail.com>
2026-04-04 17:03:58 -0400

    Avoid plotly import during docs build

--- graynet/__init__.py
+++ graynet/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ['extract', 'roiwise_stats_indiv', 'extract_multiedge', 'load_run',
-           'get_edge_values', 'export_to_nx', 'draw3Dnx', 'parcellate',
+           'get_edge_values', 'export_to_nx', 'parcellate',
            'freesurfer', 'read_freesurfer_atlas', '__version__']
 
 from importlib.metadata import PackageNotFoundError, version
@@ -8,7 +8,6 @@ from graynet import parcellate, freesurfer
 from graynet.api import extract, roiwise_stats_indiv, extract_multiedge
 from graynet.parcellate import read_freesurfer_atlas
 from graynet.results import load_run, get_edge_values, export_to_nx
-from graynet.vis_network import draw3Dnx
 
 try:
     __version__ = version('graynet')
